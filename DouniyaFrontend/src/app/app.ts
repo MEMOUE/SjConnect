c005eb7616861ffd1ea6befa0a6081b8df468b43
MEMOUE <memoue07@gmail.com>
2025-10-08 15:16:40 +0000

    feat: add header projet

--- DouniyaFrontend/src/app/app.ts
+++ DouniyaFrontend/src/app/app.ts
@@ -1,14 +1,24 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule, ButtonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('DouniyaFrontend');
-  protected readonly ButtonModule = ButtonModule;
+  protected readonly title = signal('DouniyaConnect');
+  protected mobileMenuOpen = signal(false);
+  protected routerLinkActiveOptions = { exact: true };
+
+  toggleMobileMenu() {
+    this.mobileMenuOpen.set(!this.mobileMenuOpen());
+  }
+
+  closeMobileMenu() {
+    this.mobileMenuOpen.set(false);
+  }
 }
