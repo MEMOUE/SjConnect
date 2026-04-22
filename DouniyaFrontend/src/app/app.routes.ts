328e68985d38832fbc56fcbc9ca73ce83192d66d
MEMOUE <memoue07@gmail.com>
2026-04-22 14:27:02 +0000

    fix: mise a jour des fichier docker

--- DouniyaFrontend/src/app/app.routes.ts
+++ DouniyaFrontend/src/app/app.routes.ts
@@ -93,7 +93,7 @@ export const routes: Routes = [
     path: 'accept-invitation',
     component: AcceptInvitation,
   },
-
+ 
   // Route par défaut
   { path: '**', redirectTo: '' }
 ];
--- DouniyaFrontend/src/environments/environment.ts
+++ DouniyaFrontend/src/environments/environment.ts
@@ -1,4 +1,10 @@
+// export const environment = {
+//   production: false,
+//   apiUrl: 'http://localhost:8080/api'
+// };
+
+
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8080/api'
+  production: true,
+  apiUrl: 'https://api.duniyaconnect.com/api'
 };
