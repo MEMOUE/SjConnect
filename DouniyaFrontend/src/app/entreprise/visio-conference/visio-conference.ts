d4390a623953a7d72cfc98cb962de98e8e377294
MEMOUE <memoue07@gmail.com>
2026-08-08 00:17:04 +0000

    fix:mise a jour des logo et personnalisation de l'interface de meeting

--- DouniyaFrontend/src/app/entreprise/visio-conference/visio-conference.ts
+++ DouniyaFrontend/src/app/entreprise/visio-conference/visio-conference.ts
@@ -241,9 +241,17 @@ export class VisioConference implements OnInit, OnDestroy, AfterViewInit {
         ]
       },
       interfaceConfigOverwrite: {
-        SHOW_JITSI_WATERMARK: false,
-        SHOW_WATERMARK_FOR_GUESTS: false,
+        SHOW_JITSI_WATERMARK: true,
+        SHOW_WATERMARK_FOR_GUESTS: true,
+        JITSI_WATERMARK_LINK: 'https://duniyaconnect.com',
+        DEFAULT_LOGO_URL: 'https://duniyaconnect.com/logo.png',
+        SHOW_BRAND_WATERMARK: false,
+        SHOW_POWERED_BY: false,
+        MOBILE_APP_PROMO: false,
+        HIDE_DEEP_LINKING_LOGO: true,
         APP_NAME: 'DouniyaConnect Meeting',
+        NATIVE_APP_NAME: 'DouniyaConnect Meeting',
+        PROVIDER_NAME: 'DouniyaConnect',
         DEFAULT_BACKGROUND: '#0f2855',
         TOOLBAR_ALWAYS_VISIBLE: false
       }
